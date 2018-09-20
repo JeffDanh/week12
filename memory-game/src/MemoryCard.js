3e6ff8697c01a0d7d1e09a3b20fe2b1b0024923d
Jeff Duong <jeffdanh@gmail.com>
2018-09-20 01:59:11 -0500

    memory game part 3

--- memory-game/src/MemoryCard.js
+++ memory-game/src/MemoryCard.js
@@ -2,29 +2,20 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
-    constructor(){
-        super();
-        this.state = { isFlipped:false };
-
-    }
-    clickHandler(){
-        this.setState({
-            isFlipped : !this.state.isFlipped
-        });
-    }
+    
     render(){
         var memoryCardInnerClass = "MemoryCardInner";
-        if(this.state.isFlipped){
+        if(this.props.isFlipped){
             memoryCardInnerClass += " flipped";
         }
         return(
-            <div className="MemoryCard" onClick={this.clickHandler.bind(this)}>
+            <div className="MemoryCard" onClick={this.props.pickCard}>
                 <div className={memoryCardInnerClass}>
                     <div className="MemoryCardBack">
                         <img src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png"></img>
                     </div>
                     <div className="MemoryCardFront">
-                        ∆
+                        {this.props.symbol}
                     </div>
                 </div>
             </div>
